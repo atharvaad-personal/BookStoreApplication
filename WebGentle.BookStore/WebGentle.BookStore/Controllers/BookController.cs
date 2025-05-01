008caca7e38e7b3d32ecfdccbe61b8f7582d8c20
atharvaad-personal <73948504+atharvaad-personal@users.noreply.github.com>
2025-05-01 16:22:36 +0530

    Updated the code

---
+++ WebGentle.BookStore/WebGentle.BookStore/Controllers/BookController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebGentle.BookStore.Models;
+using WebGentle.BookStore.Repository;
+
+namespace WebGentle.BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        public readonly BookRepository _bookRepository = null;
+        public BookController() 
+        { 
+            _bookRepository = new BookRepository();
+
+        } 
+       public List<BookModel> GetAllBooks()
+        {
+            return _bookRepository.GetAllBooks();
+        }
+
+        public BookModel GetBook(int id)
+        {
+            return _bookRepository.GetBookById(id);
+        }
+
+        public List<BookModel> SearchBooks(string bookName, string authorName)
+        {
+            return _bookRepository.SearchBooks(bookName, authorName);
+        }
+    }
+}
